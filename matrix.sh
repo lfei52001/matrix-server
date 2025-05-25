5a5abe65cdf62192a83e072b19934ca68775284d
lfei52001 <123644621+lfei52001@users.noreply.github.com>
2025-05-25 20:27:30 +0800

    Update matrix.sh

--- matrix.sh
+++ matrix.sh
@@ -187,7 +187,7 @@ sed -i "/database:/,/database:/ s|name: sqlite3|name: psycopg2|" synapse_data/ho
 sed -i "/database:/,/database:/ s|database: /data/homeserver.db|user: synapse_user\n    password: \"${POSTGRES_PASSWORD_ESCAPED}\"\n    database: synapse\n    host: postgres\n    cp_min: 5\n    cp_max: 10|" synapse_data/homeserver.yaml
 cat >> synapse_data/homeserver.yaml << EOF
 enable_registration: true
-max_upload_size: 1024m
+max_upload_size: 1024M
 logging:
   handlers:
     console:
@@ -336,14 +336,14 @@ volumes:
 EOF
 mkdir -p /var/lib/docker/volumes/nginx_vhost/_data
 cat > /var/lib/docker/volumes/nginx_vhost/_data/${MATRIX_DOMAIN} << EOF
-client_max_body_size 1024m;
+client_max_body_size 1024M;
 location /.well-known/matrix/server {
     return 200 '{"m.server": "${MATRIX_DOMAIN}:443"}';
 }
 EOF
 if [ "$ENABLE_SYNAPSE_ADMIN" = "y" ]; then
   cat > /var/lib/docker/volumes/nginx_vhost/_data/${ADMIN_DOMAIN} << EOF
-client_max_body_size 10m;
+client_max_body_size 10M;
 EOF
 fi
 docker compose up -d
